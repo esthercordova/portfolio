3780fd2baaa07d742432fd7577703a54d2e61d1a
esthercordova <estherpiacordova@gmail.com>
2016-07-17 19:18:43 -0400

    Initial commit

---
+++ js/vendor/video/video-vimeo.js
@@ -0,0 +1,11 @@
+$(function(){
+  $.okvideo({
+      source: '63032103',
+	autoplay: true,
+      volume: 50,
+	controls: false,
+	captions: false,
+	disablekeyControl: true
+  });
+});
+
---
+++ js/vendor/video/video-youtube.js
@@ -0,0 +1,11 @@
+$(function(){
+  $.okvideo({
+      source: 'Zl6xwuBJVIY',
+      volume: 0,
+	controls: false,
+	captions: false,
+	autoplay: true,
+	disablekeyControl: true
+  });
+});
+
